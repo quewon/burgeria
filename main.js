f587e09590ced92ae64502eddf9cdcfea74d3588
quewon <36914642+quewon@users.noreply.github.com>
2026-05-25 03:09:12 +0200

    clearer input carets

--- main.js
+++ main.js
@@ -40,53 +40,7 @@ async function init() {
         if (e.ctrlKey || e.metaKey) return;
         if (kitchen_zone.classList.contains("hidden")) return;
 
-        const input = document.createElement("input");
-        new InputManager(input, "");
-
-        input.type = "text";
-        input.style.left = mouse.x + "%";
-        input.style.top = mouse.y + "%";
-        kitchen_zone.appendChild(input);
-        input.focus();
-        
-        input.oninput = () => {
-            input.size = Math.max(input.value.length, 1);
-        }
-        input.onblur = () => {
-            const value = input.value.trim()
-            if (value !== "") {
-                let x = parseFloat(input.style.left);
-                let y = parseFloat(input.style.top);
-                for (let text of value.split(" ")) {
-                    if (text == "") {
-                        x += Word.char_width;
-                        continue;
-                    }
-                    const word = new Word(x, y, text);
-                    words.push(word);
-                    const active_point = word.get_active_interaction_point();
-                    if (active_point && active_point.type != "customer") {
-                        word.interact(active_point);
-                    }
-                    x += (text.length + 1) * Word.char_width;
-                }
-            }
-            input.remove();
-        }
-
-        input.addEventListener("keydown", e => {
-            if (e.key === " " || e.key === "Enter") {
-                const len = input.value.trim().length;
-                input.blur();
-                if (e.key === " ") {
-                    mouse.x = parseFloat(input.style.left) + (len + 1) * Word.char_width;
-                    mouse.y = parseFloat(input.style.top);
-                } else if (e.key === "Enter") {
-                    mouse.x = parseFloat(input.style.left);
-                    mouse.y = parseFloat(input.style.top) + Word.char_height;
-                }
-            }
-        })
+        create_input();
     })
 
     document.addEventListener("mousemove", e => {
@@ -96,6 +50,57 @@ async function init() {
     })
 
     start_day();
+}
+
+function create_input() {
+    const input = document.createElement("input");
+    new InputManager(input, "");
+    input.type = "text";
+    input.style.left = mouse.x + "%";
+    input.style.top = mouse.y + "%";
+    kitchen_zone.appendChild(input);
+    input.focus();
+    
+    input.oninput = () => {
+        input.size = Math.max(input.value.length, 1);
+    }
+    input.onblur = () => {
+        const value = input.value.trim()
+        if (value !== "") {
+            let x = parseFloat(input.style.left);
+            let y = parseFloat(input.style.top);
+            for (let text of value.split(" ")) {
+                if (text == "") {
+                    x += Word.char_width;
+                    continue;
+                }
+                const word = new Word(x, y, text);
+                words.push(word);
+                const active_point = word.get_active_interaction_point();
+                if (active_point && active_point.type != "customer") {
+                    word.interact(active_point);
+                }
+                x += (text.length + 1) * Word.char_width;
+            }
+        }
+        input.remove();
+    }
+
+    input.addEventListener("keydown", e => {
+        if (e.key === " " || e.key === "Enter") {
+            const len = input.value.trim().length;
+            input.blur();
+            if (e.key === " ") {
+                mouse.x = parseFloat(input.style.left) + (len + 1) * Word.char_width;
+                mouse.y = parseFloat(input.style.top);
+                create_input();
+            } else if (e.key === "Enter") {
+                mouse.x = parseFloat(input.style.left);
+                mouse.y = parseFloat(input.style.top) + Word.char_height;
+                create_input();
+            }
+        }
+    })
 }
 
 function start_day() {
